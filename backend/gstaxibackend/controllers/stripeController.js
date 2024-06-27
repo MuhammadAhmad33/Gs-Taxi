dd416680e176c995a03d89a3ea934390250775cb
Createex Mac <100872369+MuhammadAhmad33@users.noreply.github.com>
2024-06-27 15:49:46 +0500

    update StripeController

--- backend/gstaxibackend/controllers/stripeController.js
+++ backend/gstaxibackend/controllers/stripeController.js
@@ -1,35 +1,26 @@
 // src/controllers/stripeController.js
 const Stripe = require('stripe');
-const Config = require('../config/config')
-const StripeCharge = require('../models/stripe');
+const Config = require('../config/config');
 require('dotenv').config();
 
 const stripe = Stripe(Config.stripeKey);
 
 const charge = async (req, res) => {
     try {
-        const { amount, currency, source } = req.body;
+        const { amount, currency } = req.body;
 
-        if (!amount || !currency || !source) {
+        if (!amount || !currency) {
             return res.status(400).send({ error: 'Missing required fields' });
         }
 
-        const stripeCharge = await stripe.charges.create({
+        const paymentIntent = await stripe.paymentIntents.create({
             amount,
             currency,
-            source,
         });
 
-        // Save the Stripe charge details to MongoDB
-        const newCharge = new StripeCharge({
-            amount: stripeCharge.amount,
-            currency: stripeCharge.currency,
-            source: stripeCharge.source.id,
-            stripeChargeId: stripeCharge.id,
+        res.status(200).send({
+            clientSecret: paymentIntent.client_secret,
         });
-        await newCharge.save();
-
-        res.status(200).send(stripeCharge);
 
     } catch (error) {
         res.status(500).send({ error: error.message });
