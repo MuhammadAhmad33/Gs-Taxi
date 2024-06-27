9956b30c625b0b3d2a62ec717676c412eac4908f
Createex Mac <100872369+MuhammadAhmad33@users.noreply.github.com>
2024-06-27 15:00:45 +0500

    Update AfricaTalking Key

--- backend/gstaxibackend/config/config.js
+++ backend/gstaxibackend/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     mongoURI: 'mongodb+srv://gstechnologybj:gstechnologybj@cluster0.zwxzruy.mongodb.net',
     AfricasTalkingUsername: 'GSTaxi',
-    AfricasTalkingApiKey: 'df0a917e4888bf803576afbc0a2e39a7dfc73195e34235231b4f07e5b32dac93',
+    AfricasTalkingApiKey: 'atsk_eddaaf3cb62731be7140e8169088b85463c6e18b5315a300ef44e7ceb421325c9d4bb18f',
     stripeKey: 'sk_test_51Mb6LjIRO7fBL2MrJ4814rftJjzEvAQe2D8t9liuD71mb35gwpD8WPLDpOFIjjB41KrEGdZaOtUDHYJAc4iRgr9a00YEvg88JL',
     fedaPaymentKey: 'sk_live_abrzKO4mz1SlSdYTliVwTLj9',
 
