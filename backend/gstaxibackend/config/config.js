3dfb9db6c8c505b7b471a16622965c98327f6060
Createex Mac <100872369+MuhammadAhmad33@users.noreply.github.com>
2024-07-22 17:00:33 +0500

    stripe key changed
    from test to live version

--- backend/gstaxibackend/config/config.js
+++ backend/gstaxibackend/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
     mongoURI: 'mongodb+srv://gstechnologybj:gstechnologybj@cluster0.zwxzruy.mongodb.net',
     AfricasTalkingUsername: 'GSTaxi',
     AfricasTalkingApiKey: 'atsk_eddaaf3cb62731be7140e8169088b85463c6e18b5315a300ef44e7ceb421325c9d4bb18f',
-    stripeKey: 'sk_test_51Mb6LjIRO7fBL2MrJ4814rftJjzEvAQe2D8t9liuD71mb35gwpD8WPLDpOFIjjB41KrEGdZaOtUDHYJAc4iRgr9a00YEvg88JL',
+    stripeKey: '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',
     fedaPaymentKey: 'sk_live_abrzKO4mz1SlSdYTliVwTLj9',
 
 };
