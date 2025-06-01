a910632a73fa79fa0ee8330420a78feb69ad730d
Muhammad Ahmad <100872369+MuhammadAhmad33@users.noreply.github.com>
2025-06-01 06:14:07 +0500

    Update config.js

--- backend/gstaxibackend/config/config.js
+++ backend/gstaxibackend/config/config.js
@@ -1,8 +1,3 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://gstechnologybj:gstechnologybj@cluster0.zwxzruy.mongodb.net',
-    AfricasTalkingUsername: 'GSTaxi',
-    AfricasTalkingApiKey: 'atsk_eddaaf3cb62731be7140e8169088b85463c6e18b5315a300ef44e7ceb421325c9d4bb18f',
-    stripeKey: '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',
-    fedaPaymentKey: 'sk_live_abrzKO4mz1SlSdYTliVwTLj9',
+module.exports = 
 
 };
